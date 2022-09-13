792ad036d9fdbffea7bb131e15fcbfd5a367304d
Rafael Santos <dev.rafaelsantos@outlook.com>
2022-09-13 08:55:42 -0300

    resolucoes modulo 1

--- Modulo 1 - Basics/solucoes/Questao_1/Questao1.py
+++
@@ -1,5 +0,0 @@
-valor_monetario = float(input('Digite um valor monetário: '))
-
-valor = valor_monetario - (valor_monetario*0.15)
-
-print(f'O novo valor é {valor}')
--- Modulo 1 - Basics/solucoes/Questao_3/Questao3.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def calcula_area(raio):
-    return math.pi * (raio**2)
-
-area = calcula_area(2)
-print(area)
